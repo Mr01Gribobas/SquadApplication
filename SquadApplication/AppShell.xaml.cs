b9add81b2a82668b366854b5c84de4e5b3313326
Mr01Gribobas <maks-korzun@bk.ru>
2025-11-23 21:24:35 +0300

    Добавьте файлы проекта.

---
+++ SquadApplication/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SquadApplication
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SquadApplication/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SquadApplication
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
