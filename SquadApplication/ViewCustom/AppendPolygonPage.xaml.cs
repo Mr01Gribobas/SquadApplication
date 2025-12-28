49edfe8bab297ed5c6eb04b96886f812f54b7809
Mr01Gribobas <maks-korzun@bk.ru>
2025-12-29 00:08:55 +0300

    куафсещк

--- SquadApplication/ViewCustom/AppendPolygonPage.xaml.cs
+++ SquadApplication/ViewCustom/AppendPolygonPage.xaml.cs
@@ -1,9 +1,17 @@
+using AndroidX.Lifecycle;
+
 namespace SquadApplication.ViewCustom;
 
 public partial class AppendPolygonPage : ContentPage
 {
-	public AppendPolygonPage()
+    private readonly UserModelEntity _user;
+    private	 AppendPolygonViewModel _viewModel;
+
+
+	public AppendPolygonPage(IUserSession user)
 	{
-		InitializeComponent();
+		_user = user.CurrentUser;
+        _viewModel = new AppendPolygonViewModel(this,_user);
+        InitializeComponent();
 	}
 }
---
+++ SquadApplication/ViewModels/AppendPolygonViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SquadApplication.ViewModels;
+
+public partial class AppendPolygonViewModel: ObservableObject 
+{
+    public AppendPolygonViewModel(AppendPolygonPage polygonPage , UserModelEntity user)
+    {
+
+    }
+
+}
