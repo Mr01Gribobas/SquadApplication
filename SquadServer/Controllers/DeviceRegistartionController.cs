7170f637cdbaff75438b3c50522fe91e20270810
Mr01Gribobas <maks-korzun@bk.ru>
2026-01-13 17:23:21 +0300

    Start created controller RegistrationDevice

---
+++ SquadServer/Controllers/DeviceRegistartionController.cs
@@ -0,0 +1,21 @@
+﻿using SquadServer.DTO_Classes;
+using SquadServer.Services.Service_DeviceRegistration;
+
+namespace SquadServer.Controllers;
+
+[Controller]
+public class DeviceRegistartionController:ControllerBase
+{
+    private readonly IDeviceRegistrationService _deviceRegistrationService;
+    public DeviceRegistartionController(IDeviceRegistrationService registrationService)
+    {
+        _deviceRegistrationService = registrationService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> RegistartionDevice()
+    {
+       var dtoRegistration =  await HttpContext.Request.ReadFromJsonAsync<DeviceRegistrationDTO>();
+        return default;//TODO
+    }
+}
