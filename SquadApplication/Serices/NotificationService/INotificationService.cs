15429996d57505886435d3336e94772b95d5eb91
Mr01Gribobas <maks-korzun@bk.ru>
2026-01-13 04:34:08 +0300

    add service notification

---
+++ SquadApplication/Serices/NotificationService/INotificationService.cs
@@ -0,0 +1,36 @@
+﻿namespace SquadApplication.Serices.NotificationService;
+
+public interface INotificationService
+{
+
+    /// <summary>
+    /// Запрос разрешений на уведомления
+    /// </summary>
+    Task<bool> RequestNotificationPermissionAsync();
+
+    /// <summary>
+    /// Получить текущий device token
+    /// </summary>
+    Task<string> GetDeviceTokenAsync();
+
+    /// <summary>
+    /// Зарегистрировать устройство на сервере
+    /// </summary>
+    Task<bool> RegisterDeviceAsync(string userId);
+
+    /// <summary>
+    /// Отменить регистрацию устройства
+    /// </summary>
+    Task<bool> UnregisterDeviceAsync();
+
+    /// <summary>
+    /// Обработка входящих уведомлений
+    /// </summary>
+    void HandleNotificationReceived(IDictionary<string, object> data);
+
+    /// <summary>
+    /// Обработка нажатия на уведомление
+    /// </summary>
+    void HandleNotificationClicked(IDictionary<string, object> data);
+
+}
--- SquadServer/Models/ModelsEntity/EventModelEntity.cs
+++ SquadServer/Models/ModelsEntity/EventModelEntity.cs
@@ -5,13 +5,13 @@ namespace SquadServer.Models.ModelsEntity;
 public class EventModelEntity
 {
     public int Id { get; set; }
-    public string? NameTeamEnemy { get; set; }
-    public string NamePolygon { get; set; }
-    public string Coordinates { get; set; }
-    public DateOnly Date { get; set; }
-    public TimeOnly Time{ get; set; }
+    public string? NameTeamEnemy { get; set; }//команда соперника
+    public string NamePolygon { get; set; }//название полигона
+    public string Coordinates { get; set; }//координаты полигона
+    public DateOnly Date { get; set; }//дата сбора
+    public TimeOnly Time{ get; set; }//время сбора
 
-    public int CountMembers { get; set; } = 0;
+    public int CountMembers { get; set; } = 0;//колл-во сокомандников которые будет на игре 
 
 
     public int TeamId { get; set; }
