ce094549a37639389ec7f76534542e194176ac5c
MrGribobas <maks-korzun@bk.ru>
2026-01-14 20:21:47 +0300

    Append DeviceTokenManager

---
+++ SquadApplication/Services/DeviceTokenService/IDeviceTokenManager.cs
@@ -0,0 +1,33 @@
+﻿namespace SquadApplication.Services.DeviceTokenService;
+
+public interface IDeviceTokenManager
+{
+    string GetOrCreateInstallationId();
+    string GenerateDeviceToken();
+    Task<string> GetPlatformAsync();
+    string GetDeviceInfo();
+}
+public class DeviceTokenManager : IDeviceTokenManager
+{
+    public string GenerateDeviceToken()
+    {
+        throw new NotImplementedException();
+    }
+
+    public string GetDeviceInfo()
+    {
+        throw new NotImplementedException();
+    }
+
+    public string GetOrCreateInstallationId()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string> GetPlatformAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+
