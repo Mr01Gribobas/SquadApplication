7054dc373747d85c33a3e2e0f05d47ba9cf131fc
MrGribobas <maks-korzun@bk.ru>
2026-01-14 20:52:15 +0300

    create method GenerateToken

--- SquadApplication/Services/DeviceTokenService/IDeviceTokenManager.cs
+++ SquadApplication/Services/DeviceTokenService/IDeviceTokenManager.cs
@@ -1,4 +1,7 @@
-﻿namespace SquadApplication.Services.DeviceTokenService;
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace SquadApplication.Services.DeviceTokenService;
 
 public interface IDeviceTokenManager
 {
@@ -9,9 +12,38 @@ public interface IDeviceTokenManager
 }
 public class DeviceTokenManager : IDeviceTokenManager
 {
+    private const string InstallationIdKey = "installation_id";
+    private const string DeviceTokenKey = "device_token";
     public string GenerateDeviceToken()
     {
-        throw new NotImplementedException();
+        var existingToken = Task.Run(async () => await SecureStorage.GetAsync(DeviceTokenKey)).Result;
+        if(!string.IsNullOrEmpty(existingToken))
+        {
+            return existingToken;
+        }
+
+        var newToken = GenerateDeviceTokenInternal();
+    }//TODO
+
+    private object GenerateDeviceTokenInternal()
+    {
+        var installationId = GetOrCreateInstallationId().ToString();
+        var platform = Task.Run(async () => await GetPlatformAsync()).Result;
+        var timestamp = DateTime.UtcNow.Ticks;
+        var baseToken = $"{platform}_{installationId}_{timestamp}";
+
+        using var sha256 = SHA256.Create();
+
+        var hash = sha256.ComputeHash(Encoding.UTF8.GetBytes(baseToken));
+        var token = $"LOCAL_{Convert.ToBase64String(hash).
+                                   Replace("/", "_").
+                                   Replace("+", "-").
+                                   Replace("=", "").
+                                   Substring(0, 32)}";
+
+
+        Console.WriteLine($"Token is created : {token}");
+        return token;
     }
 
     public string GetDeviceInfo()
