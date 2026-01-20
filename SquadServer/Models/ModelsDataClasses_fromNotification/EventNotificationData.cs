88bc1b3c8115da451c834af39e7ed13692f04d87
Mr01Gribobas <maks-korzun@bk.ru>
2026-01-20 16:51:27 +0300

    create class EventNotificationData

---
+++ SquadServer/Models/ModelsDataClasses_fromNotification/EventNotificationData.cs
@@ -0,0 +1,17 @@
+﻿namespace SquadServer.Models.ModelsDataClasses_fromNotification;
+
+public class EventNotificationData
+{
+    public int EventId { get; set; }
+    public string EventName { get; set; }
+    public DateTime EventData { get; set; }
+    public TimeSpan EventTime { get; set; }
+    public string LocationCoordinates { get; set; }
+    public string EnemyTeam { get; set; }
+    public int RequiredMembers { get; set; }
+    public string ActionUrl { get; set; }
+}
+public class TeamNotificationData
+{
+
+}
