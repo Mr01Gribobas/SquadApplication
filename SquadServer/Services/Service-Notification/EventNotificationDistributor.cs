0103fa1128771ced29367d7af843f99062c2de4b
Mr01Gribobas <maks-korzun@bk.ru>
2026-01-20 21:22:03 +0300

    update eventNotificationDistributor

--- SquadServer/Services/Service-Notification/EventNotificationDistributor.cs
+++ SquadServer/Services/Service-Notification/EventNotificationDistributor.cs
@@ -1,4 +1,6 @@
-﻿namespace SquadServer.Services.Service_Notification;
+﻿using SquadServer.DTO_Classes.DTO_Notifiation;
+
+namespace SquadServer.Services.Service_Notification;
 
 public class EventNotificationDistributor
 {
@@ -9,11 +11,52 @@ public class EventNotificationDistributor
     }
     public async Task<NotificationResult> NotifyNewEventAsync(EventModelEntity eventModelEntity, string message = null)
     {
-        message = message ??= GenerateDefaoltMessage(eventModelEntity);
+        try
+        {
+            message = message ??= GenerateDefaultEventMessage(eventModelEntity);
+            var notification = new EventNotificationDto()
+            {
+                eventId = eventModelEntity.Id,
+                EventData = eventModelEntity,
+                _title = $"New Event {eventModelEntity.NameTeamEnemy}",
+                _message = message,
+                _notificationPriority = NotificationPriority.High,
+                _data = new
+                {
+                    EventId = eventModelEntity.Id,
+                    ActionUrl = $"events/{eventModelEntity.Id}",
+                    DateGame = eventModelEntity.Date,
+                    TimeFees = eventModelEntity.Time,
+                    Location = eventModelEntity.Coordinates
+                }
+            };
+
+            var result = await _notificationService.SendToTeamAsync(
+                                   teamId: eventModelEntity.TeamId,
+                                   notification: notification);
+
+            return result;
+
+        }
+        catch(Exception ex)
+        {
+            return new NotificationResult() {
+                Success = false,
+                Message = ex.Message
+            };
+        }
     }
 
-    private string GenerateDefaoltMessage(EventModelEntity eventModelEntity)
+
+
+
+
+
+    private string GenerateDefaultEventMessage(EventModelEntity eventModelEntity)
     {
-        throw new NotImplementedException();
+        return $"NEW EVENT!!n\"" +
+            $"NamePolygon: {eventModelEntity.NamePolygon}" +
+            $"ENEMY :{eventModelEntity.NameTeamEnemy}" +
+            $"Coordinates: {eventModelEntity.Coordinates}";
     }
 }
