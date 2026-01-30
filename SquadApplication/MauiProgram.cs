36c7688798728322f8c1a7ba8fe7cdd69047683b
MrGribobas <maks-korzun@bk.ru>
2026-01-30 18:49:30 +0300

    test work notificationLocal

--- SquadApplication/MauiProgram.cs
+++ SquadApplication/MauiProgram.cs
@@ -1,37 +1,30 @@
-﻿namespace SquadApplication;
+﻿using CommunityToolkit.Maui;
 
+namespace SquadApplication;
 public static class MauiProgram
 {
     public static MauiApp CreateMauiApp()
     {
-        
         var builder = MauiApp.CreateBuilder();
-        builder
-            .UseMauiApp<App>()
-            .ConfigureFonts(fonts =>
-            {
-                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-            });
+        builder.UseMauiApp<App>().ConfigureFonts(fonts =>
+        {
+            fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+        }).UseMauiCommunityToolkit();
         builder.Services.AddSingleton<HttpClient>(cl =>
         {
             var client = new HttpClient();
-            
             client.Timeout = TimeSpan.FromSeconds(60);
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             return client;
         });
-        builder.Services.AddSingleton<IConnectivity>(sp=>  Connectivity.Current);
+        builder.Services.AddSingleton<IConnectivity>(sp => Connectivity.Current);
         builder.Services.AddSingleton<IDeviceTokenManager, DeviceTokenManager>();
         builder.Services.AddSingleton<IDeviceManager, DeviceManager>();
-
-
         builder.Services.AddSingleton<IUserSession, UserSession>();
-
 #if DEBUG
-		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
-
         return builder.Build();
     }
 }
---
+++ SquadApplication/Services/NotificationService/NotificationLocalService.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+namespace SquadApplication.Services.NotificationService;
+public  class NotificationLocalService
+{
+    public async Task ShowLocalNotification(string title , string message)
+    {
+        var toast = Toast.Make(message,ToastDuration.Long);
+        await toast.Show();
+    }
+    public async Task CheckForNotification()
+    {
+        //var httpClient = new ...
+        await ShowLocalNotification("NewTitle","New Message");
+    }
+
+
+}
