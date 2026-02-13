5a27dff7efa187c9c90786a854cf9bd04f8f985e
MrGribobas <maks-korzun@bk.ru>
2026-02-13 14:43:41 +0300

    update eventsForAllCommands

--- SquadApplication/MauiProgram.cs
+++ SquadApplication/MauiProgram.cs
@@ -11,6 +11,7 @@ public static class MauiProgram
             fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
             fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
         }).UseMauiCommunityToolkit();
+
         builder.Services.AddSingleton<HttpClient>(cl =>
         {
             var client = new HttpClient();
--- SquadApplication/ViewModels/EventsForAllCommandsView.cs
+++ SquadApplication/ViewModels/EventsForAllCommandsView.cs
@@ -4,8 +4,21 @@ public partial class EventsForAllCommandsView : ObservableObject
 {
     private readonly IUserSession _user;
 
+    [ObservableProperty]
+    private ObservableCollection<int> events;
+
     public EventsForAllCommandsView(IUserSession user)
     {
         _user = user;
+        events = new ObservableCollection<int>();
+
+    }
+
+
+    [RelayCommand]
+    public void CreateEvent()
+    {
+        events.Add(1);
+        Console.WriteLine("Create");
     }
 }
