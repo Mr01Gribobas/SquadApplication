f2364b476715fbbf700f696b0aacdaf6b198905d
MrGribobas <maks-korzun@bk.ru>
2026-02-13 19:47:52 +0300

    add DTO Auxilia dir

---
+++ SquadApplication/DTO-Classes/DTO-AuxiliaryModels/UserAllInfoStatisticDTO.cs
@@ -0,0 +1,17 @@
+﻿using SquadServer.Models.ModelsEntity.AuxiliaryModels;
+using System.Text.Json.Serialization;
+
+namespace SquadServer.DTO_Classes.DTO_AuxiliaryModels;
+
+public record class UserAllInfoStatisticDTO
+    (
+    string NamePlayer,
+    string CallSingPlayer,
+    int CountKill,
+    int CountDieds,
+    int CountFees,
+    int CountEvents,
+    DateTime LastUpdateDataStatistics,
+    string OldDataJson,
+    List<Achievement>? Achievements
+    );
