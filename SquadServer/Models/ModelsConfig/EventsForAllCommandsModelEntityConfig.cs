560576b4e5df59f2692abb85fbae233635cdfc04
MrGribobas <maks-korzun@bk.ru>
2026-02-13 16:32:53 +0300

    append model EventsForCommands

---
+++ SquadServer/Models/ModelsConfig/EventsForAllCommandsModelEntityConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace SquadServer.Models.ModelsConfig;
+
+public class EventsForAllCommandsModelEntityConfig : IEntityTypeConfiguration<EventsForAllCommandsModelEntity>
+{
+    public void Configure(EntityTypeBuilder<EventsForAllCommandsModelEntity> builder)
+    {
+        builder.HasKey(e => e.Id);
+
+        builder.HasMany(p => p.Players);
+    }
+}
--- SquadServer/Models/ModelsEntity/EventsForAllCommandsModelEntity.cs
+++ SquadServer/Models/ModelsEntity/EventsForAllCommandsModelEntity.cs
@@ -3,5 +3,14 @@
 public class EventsForAllCommandsModelEntity
 {
     public int Id { get; set; }
-      
+    public string TeamNameOrganization { get; set; } = null!;
+    public string DescriptionShort { get; set; } = null!;
+    public string? DescriptionFull { get; set; } 
+    public string CoordinatesPolygon { get; set; } = null!;
+    public string? PolygonName { get; set; } 
+
+    public int CountPlayers = 0!;
+
+    public ICollection<UserModelEntity> Players { get; set; } = new List<UserModelEntity>();
+
 }
