358cb1560d7d551a1ad07b0d1a5b85c89e49621f
maks korzun <maks-korzun@bk.ru>
2026-04-24 12:39:44 +0300

    Append class for settings is screen

---
+++ SquadApplication/Models/DeviceModels/DeviceInfoService.cs
@@ -0,0 +1,16 @@
+﻿namespace SquadApplication.Models.DeviceModels;
+
+public class DeviceInfoService
+{
+    public double _screenWidth { get; }
+    public double _screenHeight { get; }
+    public double _screenDesity { get; }
+    public DeviceInfoService()
+    {
+        var display = DeviceDisplay.Current.MainDisplayInfo;
+        _screenWidth = display.Width / display.Density;
+        _screenHeight = display.Height / display.Density;
+        _screenDesity = display.Density;
+    }
+    
+}
