d02e37322b2c8ee5ae9490ed5ecc3413f410fa87
maks korzun <maks-korzun@bk.ru>
2026-06-03 00:07:36 +0300

    create controller

---
+++ SquadServer/Controllers/NewArchitecture/ChatController.cs
@@ -0,0 +1,14 @@
+﻿namespace SquadServer.Controllers.NewArchitecture;
+
+[Route("api/chats")]
+public class ChatController:ControllerBase
+{
+    private readonly SquadDbContext _context;
+    private readonly ChatDbService _chatDbService;
+
+    public ChatController(SquadDbContext context) 
+    {
+        _context = context;
+        _chatDbService = new ChatDbService(_context);
+    }
+}
