a5f65f8c98550aa8082d09e1640b265197e9195f
maks korzun <maks-korzun@bk.ru>
2026-06-02 12:53:26 +0300

    START CREATE new db service fo new controller

---
+++ SquadServer/Repositoryes/ChatDbService.cs
@@ -0,0 +1,26 @@
+﻿namespace SquadServer.Repositoryes;
+
+public class ChatDbService : BaseDbService
+{
+    public ChatDbService(SquadDbContext squadDb) : base(squadDb) { }
+    public async Task<bool> CreateNewChat(int teamId)
+    {
+        return default;
+    }
+    public async Task<bool> CreateNewChat(List<UserModelEntity> users, UserModelEntity host)
+    {
+        return default;
+    }
+    public async Task<bool> DeleteUserFromChatById(int userId)
+    {
+        return default;
+    }
+    public async Task<bool> DeleteUserFromChatByName(string userNameOrCallSing)
+    {
+        return default;
+    }
+    public async Task<bool> DeleteChat()
+    {
+        return default;
+    }
+}
