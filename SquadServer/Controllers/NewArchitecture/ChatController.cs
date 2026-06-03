2760e8fd6e439538acd1efa99979f34123e4d17c
maks korzun <maks-korzun@bk.ru>
2026-06-03 15:07:30 +0300

    controller

--- SquadServer/Controllers/NewArchitecture/ChatController.cs
+++ SquadServer/Controllers/NewArchitecture/ChatController.cs
@@ -1,14 +1,47 @@
 ﻿namespace SquadServer.Controllers.NewArchitecture;
 
 [Route("api/chats")]
-public class ChatController:ControllerBase
+public class ChatController : ControllerBase
 {
     private readonly SquadDbContext _context;
     private readonly ChatDbService _chatDbService;
 
-    public ChatController(SquadDbContext context) 
+    public ChatController(SquadDbContext context)
     {
         _context = context;
         _chatDbService = new ChatDbService(_context);
     }
+
+    [HttpGet("TeamChat")]
+    public async Task<IActionResult> GetChatForTeam(int teamId)
+    {
+        try
+        {
+            ChatModelDTO chat = await _chatDbService.GetChatForCommand(teamId: teamId);
+            return Ok(chat);
+        }
+        catch(Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+    [HttpGet("PrivateChats")]
+    public async Task<IActionResult> GetChats(int userId)
+    {
+        try
+        {
+            List<ChatModelDTO> chats = await _chatDbService.GetAllPrivateChat(userId);
+            return Ok(chats);
+        }
+        catch(Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+    [HttpDelete("deleteChat")]
+    public async Task<bool> DeleteChat(int chatId)
+    {
+        return await  _chatDbService.DeleteChatById(chatId);
+    }
+
 }
